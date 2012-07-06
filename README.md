8a46599b40387506454dd2846da7859950e04dec
Tom de Grunt <tom@degrunt.nl>
2012-07-06 22:54:41 +0200

    Added in example

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # jsonschema
 
 Simple and fast JSON schema
+
+## Usage
+
+	var v = new Validator();
+	var instance = 4;
+	var schema = {"type": "number"};
+	console.log(v.validate(instance, schema));
---
+++ examples/test.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var util = require('util');
+var Validator = require('./lib/validator');
+
+// Address, to be embedded on Person
+var addressSchema = {
+  "id": "Simple#Address",
+  "type" : "object",
+  "properties": {
+    "lines": {
+      "type": "array",
+      "items": {"type": "string"}
+    },
+    "zip": {"type": "string"},
+    "city": {"type": "string"},
+    "country": {"type": "string"}
+  }
+};
+
+// Person model
+var schema = {
+  "id": "Simple#Person",
+  "type" : "object",
+  "properties": {
+    "name": {"type": "string"},
+    "address": {"$ref": "Simple#Address"}
+  }
+};
+
+var p = {
+  "name": "Barack Obama",
+  "address": {
+    "lines": [ "1600 Pennsylvania Avenue Northwest" ],
+    "zip": "DC 20500",
+    "city": "Washington",
+    "country": 1
+  }
+};
+
+var v = new Validator();
+v.addSchema(addressSchema, 'Simple#Address');
+
+console.log(v.validate(p, schema));
