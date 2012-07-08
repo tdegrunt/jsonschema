1065011db74f40ae40808b9198ebd96b67c735ba
Tom de Grunt <tom@degrunt.nl>
2012-07-08 22:01:22 +0200

    Renaming sample and adding an even more simple one

---
+++ examples/simple.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var util = require('util');
+var Validator = require('../lib/validator');
+
+var v = new Validator();
+var instance = 4;
+var schema = {"type": "number"};
+console.log(v.validate(instance, schema));
+console.log(v.errors);
