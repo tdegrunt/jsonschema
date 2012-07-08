432333d34811c2b0e3c21be9dd2e0aa188fcf5d1
Tom de Grunt <tom@degrunt.nl>
2012-07-08 22:42:04 +0200

    Improved error messages for array item

--- test/mixed.js
+++ test/mixed.js
@@ -31,4 +31,26 @@ describe('Mixed', function () {
     });
   });
 
+  describe('simple object with array with invalid items', function () {
+    it('should not validate', function () {
+      this.validator.validate(
+        {'name':'test', 'lines': [1]},
+        {
+          'type': 'object',
+          'properties': {
+            'name': {'type': 'string'},
+            'lines': {
+              'type': 'array',
+              'items': {'type': 'string'}
+            }
+          }
+        }
+      ).should.be.false;
+      this.validator.errors.should.have.length(1);
+      this.validator.errors[0].should.have.property('validator', 'type');
+      this.validator.errors[0].should.have.property('message', 'is not string');
+      this.validator.errors[0].should.have.property('property', 'lines');
+    });
+  });
+
 });
