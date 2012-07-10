b06e60611edf48c18446a6f7c88d23b06f27f020
Tom de Grunt <tom@degrunt.nl>
2012-07-10 21:42:52 +0200

    More robust

--- test/union.js
+++ test/union.js
@@ -14,20 +14,37 @@ describe('Union', function () {
       "type" : "string",
     };
 
+    var numSchema = {
+      "type" : "number",
+    };
+
+    var objectIdSchema = {
+      "type": "object",
+      "id": "MongoDb#ObjectId",
+      "description": "MongoDB ObjectID",
+      "properties": {
+        "id": {"type": "string"},
+        "_bsontype": {"type": "string"}
+      }
+    };
+
     this.validator.addSchema(testSchema, 'Test#Simple');
+    this.validator.addSchema(numSchema, 'Test#Num');
+    this.validator.addSchema(objectIdSchema, 'MongoDb#ObjectId');
+
   });
 
   describe('string and number', function () {
-  it('should validate for number', function () {
-    this.validator.validate(1, {'type': ['number', 'string']}).should.be.empty;
-  });
-  it('should validate for string', function () {
-    this.validator.validate('1', {'type': ['number', 'string']}).should.be.empty;
-  });
+    it('should validate for number', function () {
+      this.validator.validate(1, {'type': ['number', 'string']}).should.be.empty;
+    });
+    it('should validate for string', function () {
+      this.validator.validate('1', {'type': ['number', 'string']}).should.be.empty;
+    });
 
-  it('should not validate if no string or number', function () {
-    this.validator.validate(true, {'type': ['number', 'string']}).should.not.be.empty;
-  });
+    it('should not validate if no string or number', function () {
+      this.validator.validate(true, {'type': ['number', 'string']}).should.not.be.empty;
+    });
   });
 
   describe('string and null', function () {
@@ -40,7 +57,7 @@ describe('Union', function () {
 
     it('should not validate if no string or number', function () {
       this.validator.validate(true, {'type': ['null', 'string']}).should.not.be.empty;
-      });
+    });
   });
 
   describe('string and null', function () {
@@ -59,14 +76,74 @@ describe('Union', function () {
   describe('null and $ref', function () {
     it('should validate for null', function () {
       this.validator.validate(null, {'type': ['null', {'$ref':'Test#Simple'}]}).should.be.empty;
-      });
+    });
 
     it('should validate for string', function () {
       this.validator.validate('test', {'type': ['null', {'$ref':'Test#Simple'}]}).should.be.empty;
-      });
+    });
 
     it('should not validate if no string or number', function () {
       this.validator.validate(true, {'type': ['null', {'$ref':'Test#Simple'}]}).should.not.be.empty;
-      });
+     });
+  });
+
+  describe('$ref and string', function () {
+    it('should validate for *', function () {
+      this.validator.validate('*', {'type': [{ type: 'string', pattern: '^\\*$' }, {'$ref':'Test#Num'}]}).should.be.empty;
+    });
+
+    it('should validate for 1', function () {
+      this.validator.validate(1, {'type': [{ type: 'string', pattern: '^\\*$' }, {'$ref':'Test#Num'}]}).should.be.empty;
+    });
+
+    it('should not validate for -', function () {
+      var result = this.validator.validate('-', {'type': [{ type: 'string', pattern: '^\\*$' }, {'$ref':'Test#Num'}]});
+      result.should.not.be.empty;
+    });
+  });
+
+  describe('complex $ref and string', function () {
+    it('should validate for array or *', function () {
+      var schema = {
+        "type": "object",
+        "properties": {
+          "wildcards": {
+            "type": "array",
+            "items": {"type": [{"$ref": "MongoDb#ObjectId"}, {"type": "string", "pattern": "^\\*$"}]}
+          }
+        }
+      };
+      var validator = new Validator();
+      validator.validate({'wildcards': ['*']}, schema).should.be.empty;
+    });
+
+    it('should validate for empty array', function () {
+      var schema = {
+        "type": "object",
+        "properties": {
+          "wildcards": {
+            "type": "array",
+            "items": {"type": [{"$ref": "MongoDb#ObjectId"}, {"type": "string", "pattern": "^\\*$"}]}
+          }
+        }
+      };
+      var validator = new Validator();
+      validator.validate({'wildcards': []}, schema).should.be.empty;
+    });
+
+    it('should validate for objectid', function () {
+      var schema = {
+        "type": "object",
+        "properties": {
+          "wildcards": {
+            "type": "array",
+            "items": {"type": [{"$ref": "MongoDb#ObjectId"}, {"type": "string", "pattern": "^\\*$"}]}
+          }
+        }
+      };
+      var validator = new Validator();
+      validator.validate({'wildcards': [{"id": "1234", "_bsontype": "test"}, '*']}, schema).should.be.empty;
+    });
+
   });
 });
