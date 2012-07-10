ddaadce9a701f3f269e361ec5f5709ca733a7412
Tom de Grunt <tom@degrunt.nl>
2012-07-10 14:06:36 +0200

    Bugfix

--- test/attributes.js
+++ test/attributes.js
@@ -23,6 +23,12 @@ describe('Attributes', function () {
       });
     });
 
+    describe('required', function () {
+      it('should validate an undefined instance', function () {
+        this.validator.validate(undefined, {'type': 'number', 'required': true}).should.not.be.empty;
+      });
+    });
+
     describe('null', function () {
 
       it('should validate null', function () {
