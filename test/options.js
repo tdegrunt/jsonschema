cdacb77b2eb684dfc441773d88bfed1a950b3436
Andrii Shumada <eagleeyes91@gmail.com>
2014-07-24 10:56:07 +0300

    fix instance undefined

--- test/options.js
+++ test/options.js
@@ -20,14 +20,14 @@ describe('Options', function () {
 	});
 
 	describe('validate with option includeInstance = false', function () {
-		it('should provide instance', function () {
-			this.validator.validate(4, {"type": "string"}, {includeInstance: false}).errors.should.not.have.deep.property('0.instance');
+		it('should not provide empty instance', function () {
+			this.validator.validate(4, {"type": "string"}, {includeInstance: false}).errors.should.have.deep.property('0.instance').to.deep.eql({});
 		});
 	});
 
 	describe('validate with option includeSchema = false', function () {
-		it('should provide instance', function () {
-			this.validator.validate(4, {"type": "string"}, {includeSchema: false}).errors.should.not.have.deep.property('0.schema');
+		it('should provide empty schema', function () {
+			this.validator.validate(4, {"type": "string"}, {includeSchema: false}).errors.should.have.deep.property('0.schema').to.deep.eql({});
 		});
 	});
 });
