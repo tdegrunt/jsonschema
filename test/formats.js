9c545f5fcaa03608f66327aa615d38906c264db9
Shelby Sanders <shelbys@gmail.com>
2015-02-24 20:17:52 -0800

    Refactored to include pattern in error message for format violations

--- test/formats.js
+++ test/formats.js
@@ -275,7 +275,6 @@ describe('Formats', function () {
 
     it('should not validate empty', function () {
       var result = this.validator.validate("", {'type': 'string', 'format': 'non-empty'});
-      console.log('sss', result);
       result.valid.should.be.false;
       result.errors[0].message.should.equal('must not be empty');
     });
