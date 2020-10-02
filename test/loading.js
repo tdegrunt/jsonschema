03ca6827027fcc9bf75afa14cf11220bf9e4e67c
Austin Wright <aaa@bzfx.net>
2020-10-01 19:40:41 -0700

    scan: fix repeat definition error message

--- test/loading.js
+++ test/loading.js
@@ -5,6 +5,7 @@
 
 var Validator = require('../lib/validator');
 var should = require('chai').should();
+var assert = require('assert');
 
 var dataSchema =           require('./fixtures/data_schema.json');
 var dataCollectionSchema = require('./fixtures/data_collection_schema.json');
@@ -34,6 +35,22 @@ describe('Schema management', function testSchemaManagement() {
       this.validator.addSchema(dataCollectionSchema, '/data_collection_schema.json');
       this.validator.addSchema(typesSchema, '/types.json');
       this.validator.validate(dataCollection, '/data_collection_schema.json').valid.should.be.true;
+    });
+
+    it('Multiple schemas with the same id should throw an error', function testForCorrectValidation() {
+      var validator = this.validator;
+      assert.throws(function(){
+        validator.addSchema({
+          properties: {
+            a: { id: 'http://example.com/schema.json', type: 'string' },
+            b: { id: 'http://example.com/schema.json', type: 'number' },
+          }
+        });
+      }, function(err){
+        // This may be changed as necessary
+        err.message.should.equal('Schema <http://example.com/schema.json#> already exists with different definition');
+        return true;
+      })
     });
   });
 });
